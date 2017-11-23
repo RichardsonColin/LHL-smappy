15a27325d34f5cedeaf00011f5018b5d1e3c6d40
Colin Richardson <richardsonjcolin@gmail.com>
2017-11-23 17:26:07 +0000

    clean up code

--- server.js
+++ server.js
@@ -16,10 +16,12 @@ const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
-
 const allMapsRoutes = require("./routes/all_maps");
 const favoritesRoutes = require("./routes/favorites");
-const contributionsRoutes = require("./routes/contributions")
+const contributionsRoutes = require("./routes/contributions");
+
+app.set("view engine", "ejs");
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -27,8 +29,7 @@ app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
-
-app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
   src: __dirname + "/styles",
@@ -36,13 +37,10 @@ app.use("/styles", sass({
   debug: true,
   outputStyle: 'expanded'
 }));
-app.use(express.static("public"));
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
-
 app.use("/api/maps", allMapsRoutes(knex));
-
 app.use("/api/favorites", favoritesRoutes(knex));
 app.use("/api/contributions", contributionsRoutes(knex));
 
