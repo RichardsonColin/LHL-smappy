1511be283f9dc040523894e819e27ec09ffd5ec4
Kelsey Cooper <kcoop@live.ca>
2017-11-23 21:56:36 +0000

    set up register routes and function

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ require('dotenv').config();
 
 const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
+const cookieSession = require('cookie-session');
 const express     = require("express");
 const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");
 const app         = express();
 const bcrypt = require('bcrypt');
-const cookieSession = require('cookie-session');
 
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
@@ -60,22 +60,93 @@ app.get("/login-test", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  let userEmail = req.body.email;
-  let hashPassword = bcrypt.hashSync(req.body.password, 10);
+  const userEmail = req.body.email;
+  const password = req.body.password;
+  console.log(req.session.user_id);
 
-  knex('users')
-  .insert({
-    email: userEmail,
-    password: hashPassword
-  })
-  .returning('id')
-  .asCallback(function(err, id) {
-    console.log(id);
-  // knex.destroy(); // Ask someone about this, a new call doesn't open this back up
-  });
-  res.render("login-test");
+  res.redirect('login-test');
 });
 
+app.post("/logout", (req, res) => {
+  req.session.user_id = null;
+  // console.log(`this is when we log out ${req.session.user_id}`);
+  res.redirect('login-test');
+});
+
+
+function registerUser(email, password) {
+  return new Promise((resolve, reject) => {
+    let user_id = 0;
+    knex('users')
+      .insert({
+        email: email,
+        password: password
+      })
+      .returning('id')
+      .asCallback(function(err, id) {
+        console.log(id[0]);
+        user_id = id[0];
+        console.log(user_id);
+    });
+    return user_id;
+      // console.log('IM THE GODDAMN USER_ID BEFORE RETURN',user_id);
+    });
+}
+
+
+function checkEmailInDB(email) {
+  return new Promise((resolve, reject) => {
+    let returnEmailResult = false;
+    knex
+    .select('email')
+    .from('users')
+    .where({'email': email})
+    .asCallback(function(err, result) {
+        console.log("returning result", result.length);
+      if (result.length === 0) {
+        console.log("true");
+        returnEmailResult = true;
+      }
+    });
+    return returnEmailResult;
+  });
+}
+
+
+function checkForm (email, password) {
+ if (email || password === undefined){
+  return true;
+ } else {
+  return false;
+ }
+}
+
+
+app.post('/register', (req, res) => {
+  const email = req.body.email;
+  const password = bcrypt.hashSync(req.body.password, 10);
+  const formCheck = checkForm(email, password);
+
+  if (formCheck) {
+    //MOVE ON
+    let emailCheck = checkEmailInDB(email);
+      console.log("this should be true", emailCheck);
+    if (emailCheck) {
+      let id = registerUser(email, password);
+      req.session.user_id = id;
+      console.log(`I am the id that equals the user_id ${id}`);
+      // console.log(`this is when the register ends ${req.session.user_id}`);
+    } else {
+      res.send("You already have an email in the db FINISH THIS MESSAGE LATER!!");
+    }
+
+
+  } else {
+    res.send("You need to fill out the email \& password, KELSEY and REID UPDATE THIS SO ITDOESNT SEND YOU TO A DIF PAGE");
+  }
+
+  res.redirect("login-test");
+});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
