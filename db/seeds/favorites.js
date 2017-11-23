dfa0ffcb012f69fc5c32c2dd1ec41db36f786004
MarkZsombor <markszsombor@gmail.com>
2017-11-23 19:24:26 +0000

    cleaned up code for seed files

--- db/seeds/favorites.js
+++ db/seeds/favorites.js
@@ -1,6 +1,6 @@
 
 exports.seed = function(knex, Promise) {
-        return knex('favourite_maps').insert([
-        {user_id: 1, map_id: 1}
-      ]);
+  return knex('favourite_maps').insert([
+    {user_id: 1, map_id: 1}
+  ]);
 };
