08da2b8fb157213f1f4096b3ba3bc52970f71320
Colin Richardson <richardsonjcolin@gmail.com>
2017-11-23 22:15:43 +0000

    fix left slide toggle button for leftnav

--- public/scripts/toggle.js
+++ public/scripts/toggle.js
@@ -3,12 +3,9 @@ $(() => {
     var arrowButton = $('.arrow-button');
 
     arrowButton.click(function() {
-      $('.left-nav').toggle('slide');
-      //event.preventDefault;
-    //   $(textForm).slideToggle( "slow", function() {
-    //     $('.tweet-text').focus();
-    //   });
-    // });
+      $('.left-nav').toggle('slide', function() {
+        $('.fa-arrow-circle-left').toggleClass('fa-rotate-180');
+      });
     });
   }
   toggleLeftNav();
