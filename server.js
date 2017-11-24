23c3d39b0a052fb2a3f09c0937d181c96138a866
Kelsey Cooper <kcoop@live.ca>
2017-11-24 01:32:25 +0000

    register route hooked up to promises

--- server.js
+++ server.js
@@ -55,98 +55,95 @@ app.get("/", (req, res) => {
 });
 
 app.get("/login-test", (req, res) => {
-  console.log("hello");
+  // console.log("hello");
   res.render("login-test");
 });
 
 app.post("/login", (req, res) => {
-  const userEmail = req.body.email;
-  const password = req.body.password;
-  console.log(req.session.user_id);
+  const emailReq = req.body.email;
+  const passwordReq = req.body.password;
+  let pass;
+  knex('users').where({
+  email: emailReq }).select('password').then(
+  function(result){
+      pass = result;
+      }).catch(function(error) {
+  console.log(error);
+  });
 
+  if (passwordReq === pass){
+    //login
+  }
   res.redirect('login-test');
 });
 
 app.post("/logout", (req, res) => {
+  console.log('logout id',req.session.user_id);
   req.session.user_id = null;
-  // console.log(`this is when we log out ${req.session.user_id}`);
+  console.log('logout id after logout',req.session.user_id);
   res.redirect('login-test');
 });
 
 
 function registerUser(email, password) {
-  return new Promise((resolve, reject) => {
-    let user_id = 0;
-    knex('users')
-      .insert({
-        email: email,
-        password: password
-      })
-      .returning('id')
-      .asCallback(function(err, id) {
-        console.log(id[0]);
-        user_id = id[0];
-        console.log(user_id);
+  let user_id = 0;
+
+  return knex('users')
+    .insert({
+      email: email,
+      password: password
+    })
+    .returning('*')
+    .then((users) => {
+
+      console.log(users[0].id);
+      user_id = users[0].id;
+
+      return user_id;
     });
-    return user_id;
+      // console.log('before return',user_id);
+    // });
       // console.log('IM THE GODDAMN USER_ID BEFORE RETURN',user_id);
-    });
 }
 
 
 function checkEmailInDB(email) {
-  return new Promise((resolve, reject) => {
-    let returnEmailResult = false;
-    knex
-    .select('email')
-    .from('users')
-    .where({'email': email})
-    .asCallback(function(err, result) {
-        console.log("returning result", result.length);
-      if (result.length === 0) {
-        console.log("true");
-        returnEmailResult = true;
-      }
-    });
-    return returnEmailResult;
-  });
+  // return new Promise((resolve, reject) => {
+
+    return knex
+      .select('email')
+      .from('users')
+      .where({'email': email})
+      .then((users) => {
+        return users.length !== 0;
+      });
 }
 
 
-function checkForm (email, password) {
- if (email || password === undefined){
-  return true;
- } else {
-  return false;
- }
-}
+
 
 
 app.post('/register', (req, res) => {
   const email = req.body.email;
   const password = bcrypt.hashSync(req.body.password, 10);
-  const formCheck = checkForm(email, password);
+  // checkEmailInDB(email, password);
 
-  if (formCheck) {
-    //MOVE ON
-    let emailCheck = checkEmailInDB(email);
-      console.log("this should be true", emailCheck);
-    if (emailCheck) {
-      let id = registerUser(email, password);
-      req.session.user_id = id;
-      console.log(`I am the id that equals the user_id ${id}`);
-      // console.log(`this is when the register ends ${req.session.user_id}`);
+  //let check = registerUser(email, password);
+  checkEmailInDB(email, password)
+  .then(exists => {
+    if (!exists) {
+      return registerUser(email, password)
+      .then(user_id => {
+        req.session.user_id = user_id;
+        console.log('right after registration ',req.session.user_id);
+        res.redirect('login-test');
+      });
     } else {
-      res.send("You already have an email in the db FINISH THIS MESSAGE LATER!!");
+      res.status(400).send('Email has already been registered');
     }
-
-
-  } else {
-    res.send("You need to fill out the email \& password, KELSEY and REID UPDATE THIS SO ITDOESNT SEND YOU TO A DIF PAGE");
-  }
-
-  res.redirect("login-test");
+  });
 });
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
