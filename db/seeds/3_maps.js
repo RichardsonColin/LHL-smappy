172fed76ee88bcfeeddfe689a51c86ff54995cf7
MarkZsombor <markszsombor@gmail.com>
2017-11-24 23:29:43 +0000

    made new markers seeds

--- db/seeds/3_maps.js
+++ db/seeds/3_maps.js
@@ -3,6 +3,6 @@ exports.seed = function(knex, Promise) {
   return knex('maps').insert([
     {id: 1, title: 'map dummy title', user_id: 1},
     {id: 2, title: 'Antartica pengus', user_id: 1},
-    {id: 3, title: 'This is a title', user_id: 1, lat: 48.425, long: -123.3630, zoom: 14}
+    {id: 3, title: 'Spots I saw animals', user_id: 1, lat: 48.425, long: -123.3630, zoom: 14}
   ]);
 };
