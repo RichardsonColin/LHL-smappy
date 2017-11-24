46f7855b2ff5a4088e29ed7f7550507fe9a693b7
Colin Richardson <richardsonjcolin@gmail.com>
2017-11-24 22:36:18 +0000

    add a full map with search input and title input

--- public/scripts/new-map.js
+++ public/scripts/new-map.js
@@ -1,62 +1,68 @@
-// All commented out code is for autofilling an address fields. Until it is sure
-// we won't need that extra code, it will remain.
+// Commented out code relates to markers on the map that at this moment is not needed.
 
-var placeSearch, autocomplete;
-      // var componentForm = {
-      //   street_number: 'short_name',
-      //   route: 'long_name',
-      //   locality: 'long_name',
-      //   administrative_area_level_1: 'short_name',
-      //   country: 'long_name',
-      //   postal_code: 'short_name'
-      // };
+function initAutocomplete() {
+        var map = new google.maps.Map(document.getElementById('googleMap'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13,
+          mapTypeId: 'roadmap'
+        });
 
-      function initAutocomplete() {
-        // Create the autocomplete object, restricting the search to geographical
-        // location types.
-        autocomplete = new google.maps.places.Autocomplete(
-            /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
-            {types: ['geocode']});
+        // Create the search box and link it to the UI element.
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-        // When the user selects an address from the dropdown, populate the address
-        // fields in the form.
-        autocomplete.addListener('place_changed'/*, fillInAddress*/);
-      }
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
 
-      // function fillInAddress() {
-      //   // Get the place details from the autocomplete object.
-      //   var place = autocomplete.getPlace();
+        //var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
 
-      //   for (var component in componentForm) {
-      //     document.getElementById(component).value = '';
-      //     document.getElementById(component).disabled = false;
-      //   }
+          if (places.length == 0) {
+            return;
+          }
 
-      //   // Get each component of the address from the place details
-      //   // and fill the corresponding field on the form.
-      //   for (var i = 0; i < place.address_components.length; i++) {
-      //     var addressType = place.address_components[i].types[0];
-      //     if (componentForm[addressType]) {
-      //       var val = place.address_components[i][componentForm[addressType]];
-      //       document.getElementById(addressType).value = val;
-      //     }
-      //   }
-      // }
+          // Clear out the old markers.
+          // markers.forEach(function(marker) {
+          //   marker.setMap(null);
+          // });
+          // markers = [];
 
-      // Bias the autocomplete object to the user's geographical location,
-      // as supplied by the browser's 'navigator.geolocation' object.
-      function geolocate() {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var geolocation = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
             };
-            var circle = new google.maps.Circle({
-              center: geolocation,
-              radius: position.coords.accuracy
-            });
-            autocomplete.setBounds(circle.getBounds());
+
+            // Create a marker for each place.
+            // markers.push(new google.maps.Marker({
+            //   map: map,
+            //   icon: icon,
+            //   title: place.name,
+            //   position: place.geometry.location
+            // }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
           });
-        }
+          map.fitBounds(bounds);
+        });
       }
