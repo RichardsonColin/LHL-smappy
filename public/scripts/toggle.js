373e8b19856b866f9f70e6368620f16bf603256e
Kelsey Cooper <kcoop@live.ca>
2017-11-25 19:52:45 +0000

    Added toggle to arrow on left nav toggle

--- public/scripts/toggle.js
+++ public/scripts/toggle.js
@@ -1,9 +1,35 @@
 $(() => {
 
-  $(".left-nav").click(function () {
+function toggleLeftNav() {
+    $(".left-nav").click(function () {
     tp = $(this).css('left') == '0px' ? '-350px' : '0px';
     $(this).animate( {left: tp }, 1000);
-});
+
+toggleLeftNavArrow();
+
+  });
+}
+toggleLeftNav();
+
+let rotated = false;
+
+function toggleLeftNavArrow(){
+
+  console.log(rotated);
+  if (!rotated) {
+    $(".left-nav").find('.fa').css({"transform": "rotate(180deg)"});
+    rotated = true;
+    console.log(rotated);
+    console.log("rotate 180");
+  } else {
+    $(".left-nav").find('.fa').css({"transform": "rotate(0deg)"});
+    rotated = false;
+    console.log("after");
+  }
+}
+
+  // console.log($(".left-nav").find('.fa'));
+
 
 
 function toggleLogin() {
