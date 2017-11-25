9914a265a30e82ac544753c6c314c6985878f7da
MarkZsombor <markszsombor@gmail.com>
2017-11-25 20:01:12 +0000

    clicking the map makes a new marker and opens the marker info box

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -10,140 +10,24 @@ function closeInfoWindows() {
 function initMap() {
 let mapData = {};
 
-if (!map_data) {
-  mapData = {
-    lat: 49.28,
-    long: -123.11,
-    zoom: 14
-  };
-} else {
   let importData = JSON.parse(map_data);
 
-  console.log(importData);
+  // console.log(importData);
   drawMap(importData);
 
-}
 
-  // var map = new google.maps.Map(document.getElementById("googleMap"), {
+  // google.maps.event.addListener(map.data, 'click', function(e) {
+  //   if (e.feature.getGeometry().getType() === 'Point') {
 
-  //   center: new google.maps.LatLng(mapData.lat, mapData.long),
-  //   zoom: mapData.zoom,
-  //   noClear: true
-
-  //   }),
-
-
-
-  //   // this may be the stored data
-    data = {
-      "type": "FeatureCollection",
-      "features": [{
-        "type": "Feature",
-        "geometry": {
-          "type": "Point",
-          "coordinates": []
-        },
-        "properties": {}
-      }]
-    },
-    win = new google.maps.InfoWindow,
-
-
-    //some buttons for interaction
-    ctrl = document.getElementById('datactrl'),
-
-
-    fx = {
-      'data-save': {
-        click: function() {
-          //use this method to store the data somewhere,
-          //e.g. send it to a server
-          console.log('map.data', map.data);
-          console.log('data', data);
-          map.data.toGeoJson(function(json) {
-            data = json;
-          });
-
-        }
-      },
-      'data-show': {
-        click: function() {
-          alert('you may send this JSON-string to a server and store it there:\n\n' +
-            JSON.stringify(data))
-          console.log(data);
-        }
-      },
-      'data-load': {
-        click: function() {
-          //use this method to load the data from somwhere
-          //e.g. from a server via loadGeoJson
-
-          map.data.forEach(function(f) {
-            map.data.remove(f);
-          });
-          map.data.addGeoJson(data)
-        },
-        init: true
-      },
-      'data-clear': {
-        click: function() {
-          //use this method to clear the data
-          //when you also want to remove the data on the server
-          //send a geoJSON with empty features-array to the server
-
-          map.data.forEach(function(f) {
-            map.data.remove(f);
-          });
-          data = {
-            type: "FeatureCollection",
-            features: []
-          };
-
-
-        }
-      }
-    };
-
-
-  // for (var id in fx) {
-  //   var o = ctrl.querySelector('input[id=' + id + ']');
-  //   google.maps.event.addDomListener(o, 'click', fx[id].click);
-  //   if (fx[id].init) {
-  //     google.maps.event.trigger(o, 'click');
+  //     win.setOptions({
+  //       content: 'Latitude: ' + e.feature.getGeometry().get().lat() +
+  //         '<br>Longitude: ' + e.feature.getGeometry().get().lng(),
+  //       pixelOffset: new google.maps.Size(0, -40),
+  //       map: map,
+  //       position: e.feature.getGeometry().get()
+  //     });
   //   }
-  // }
-
-
-// console.log('map', map);
-
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(ctrl);
-
-
-
-
-  function placeMarker(location) {
-    var feature = new google.maps.Data.Feature({
-      geometry: location
-    });
-    map.data.add(feature);
-  }
-  google.maps.event.addListener(map, 'click', function(event) {
-    placeMarker(event.latLng);
-  });
-
-
-  google.maps.event.addListener(map.data, 'click', function(e) {
-    if (e.feature.getGeometry().getType() === 'Point') {
-
-      win.setOptions({
-        content: 'Latitude: ' + e.feature.getGeometry().get().lat() +
-          '<br>Longitude: ' + e.feature.getGeometry().get().lng(),
-        pixelOffset: new google.maps.Size(0, -40),
-        map: map,
-        position: e.feature.getGeometry().get()
-      });
-    }
-  });
+  // });
 
 
   // });
@@ -152,43 +36,39 @@ if (!map_data) {
 function drawMap (data) {
   let markerArr = [];
   let mapData = data.map_data1;
-  console.log('in drawmap', mapData);
+  // console.log('in drawmap', mapData);
   // document.querySelector('#mapTitleInput').value = mapData.title;
   let pointsData = data.markers_input;
-  console.log('indrawmap', pointsData);
+  // console.log('indrawmap', pointsData);
 
   let mapOptions = {
     center: new google.maps.LatLng(mapData.lat, mapData.long),
     zoom: Number(mapData.zoom),
-    // mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
 
   };
 
-  map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+  const map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 
   pointsData.forEach(function (point) {
-    console.log(point);
+    // console.log(point);
     let latLng = new google.maps.LatLng(point.lat, point.long);
     let infoBox = `<p>${point.title}</p> <p>${point.description}</p>`;
-
-    // {
-    //   title: point.title,
-    //   description: point.description
-    // }
     let marker = new google.maps.Marker({
       position: latLng,
       map: map,
       infoBox: infoBox
     });
-    console.log('marker', marker.infoBox);
+    // console.log('marker', marker.infoBox);
     google.maps.event.addListener(marker, "click", function () {
       closeInfoWindows();
+      console.log('clicked a marker');
       var content = marker.infoBox;
       var infoWindow = new google.maps.InfoWindow({
         content: content
       });
       allInfoWindows.push(infoWindow);
-      console.log('all infor windows', allInfoWindows);
+      // console.log('all infor windows', allInfoWindows);
       infoWindow.open(map, marker);
     });
   });
@@ -196,27 +76,24 @@ function drawMap (data) {
 
 
 
-  //   let thisMarker = marker(point.lat, point.lng, map, point.name, point.description, point.img_url);
-  //   thisMarker['infoBox'] = {
-  //     title: point.name,
-  //     description: point.description,
-  //     url: point.img_url
-  //   };
-    // markerArr.push(thisMarker);
-  // });
 
-  // return map;
+  //Attach click event handler to the map.
+  google.maps.event.addListener(map, 'click', function (e) {
+
+    //Determine the location where the user has clicked.
+    var location = e.latLng;
+    console.log('e.fa.x', e.fa.x);
+    console.log('e.fa.y', e.fa.y);
+
+    //Create a marker and placed it on the map.
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map
+    });
+
+    $(".marker-info").css('visibility', 'visible');
+
+  });
 
 }
 
-
-     // window.eqfeed_callback = function(results) {
-     //    for (var i = 0; i < results.features.length; i++) {
-     //      var coords = results.features[i].geometry.coordinates;
-     //      var latLng = new google.maps.LatLng(coords[1],coords[0]);
-     //      var marker = new google.maps.Marker({
-     //        position: latLng,
-     //        map: map
-     //      });
-     //    }
-     //  }
