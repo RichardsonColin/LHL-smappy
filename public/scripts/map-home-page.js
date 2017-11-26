d6d167e6231079f6a3ab98090034552ba439776b
MarkZsombor <markszsombor@gmail.com>
2017-11-26 23:40:01 +0000

    cut out unneeded markers leftover text

--- public/scripts/map-home-page.js
+++ public/scripts/map-home-page.js
@@ -1,22 +1,6 @@
 
-function initMap() {
-let mapData = {};
 
-  let importData = JSON.parse(map_data);
-
-  // console.log(importData);
-  drawMap(importData);
-
-}
-
-
-function drawMap (data) {
-  let markerArr = [];
-  let mapData = data.map_data1;
-  // console.log('in drawmap', mapData);
-  // document.querySelector('#mapTitleInput').value = mapData.title;
-  let pointsData = data.markers_input;
-  // console.log('indrawmap', pointsData);
+function initMap () {
 
   let mapOptions = {
     center: {lat: 52.596678, lng: -134.757003},
@@ -26,38 +10,6 @@ function drawMap (data) {
   };
 
   const map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
-
-  pointsData.forEach(function (point) {
-    // console.log(point);
-    let latLng = new google.maps.LatLng(point.lat, point.long);
-    let infoBox = `<p>${point.title}</p> <p>${point.description}</p>`;
-    let marker = new google.maps.Marker({
-      position: latLng,
-      map: map,
-      databaseId: point.id,
-      infoBox: infoBox
-    });
-
-    marker.id = uniqueId;
-    uniqueId++;
-    // console.log('marker', marker.infoBox);
-    google.maps.event.addListener(marker, "click", function () {
-      closeInfoWindows();
-      console.log('clicked a marker');
-      console.log(marker.id);
-      var content = marker.infoBox;
-      var infoWindow = new google.maps.InfoWindow({
-        content: content
-      });
-      allInfoWindows.push(infoWindow);
-      // console.log('all infor windows', allInfoWindows);
-      infoWindow.open(map, marker);
-    });
-
-    markers.push(marker);
-  });
-
-  console.log(markers);
 
 }
 
