07d52211186825ec09cfa659045906dcf8e693d6
Kelsey Cooper <kcoop@live.ca>
2017-11-26 20:34:58 +0000

    created ajax request to server to update the profole, completed the route on server

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -16,7 +16,7 @@ $(() => {
     method: "GET",
     url: `/api/users/`
   }).done((users) => {
-    for(user of users) {
+    for(let user of users) {
       $("<h1>").text(user.email).prependTo($(".profile-container"));
       $("<h2>").text(user.your_location).prependTo($(".profile-container"));
     }
@@ -26,7 +26,7 @@ $(() => {
     method: "GET",
     url: "/api/favorites"
   }).done((favorites) => {
-    for(map of favorites) {
+    for(let map of favorites) {
     console.log(map.title);
       $("<div>").text(map.title).appendTo($(".favorite"));
     }
@@ -37,8 +37,33 @@ $(() => {
     url: "/api/contributions"
   }).done((contributions) => {
     console.log(contributions);
-    for(map of contributions) {
+    for(let map of contributions) {
       $("<div>").text(map.title).appendTo($(".contributions"));
     }
   });
+
+  $( '#profile-form' ).on('submit', function(event) {
+    event.preventDefault();
+    const $form = $(this).parent();
+    let $name = $($form).find('input[name="user_name"]').val();
+    let $location = $($form).find('input[name="user_location"]').val();
+    let $description = $($form).find('textarea').val();
+
+    let data = {name: $name, location: $location, description: $description};
+
+    if ($name.length === 0 || $location.length === 0 || $description.length === 0) {
+      $.flash("All profile information must be filled out to complete your profile");
+    } else if ($description.length > 200) {
+      $.flash("We know you have a lot to say about yourself but try to keep it under 200 chars!");
+    } else {
+      $.ajax({
+              type: 'POST',
+              url:  '/profile-update',
+              data: data,
+      });
+    console.log($name, $location, $description);
+    }
+  });
+
+
 });
