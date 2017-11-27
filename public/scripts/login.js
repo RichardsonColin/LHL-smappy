d61562dd5a136d0c2401929ddf9902dec0d458ab
reidnaa <reidnaaykens@yahoo.ca>
2017-11-27 18:24:27 +0000

    Login drop down will display email instead of user name

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -8,7 +8,7 @@ $(() => {
     url: `/api/users/`
   }).done((users) => {
     for(user of users) {
-      $("<span>").text(user.name).prependTo($(".pop-down-logout"));
+      $("<span>").text(user.email).prependTo($(".pop-down-logout"));
       $("<span>").text(user.your_location).prependTo($(".user-local"));
     }
   });
