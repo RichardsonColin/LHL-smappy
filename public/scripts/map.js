07619de5d20b8f989656b1d261485fc2832d3131
MarkZsombor <markszsombor@gmail.com>
2017-11-27 17:03:28 +0000

    consolodated the map page js into one file, added comments and fixed formatting

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -168,7 +168,78 @@ var mapData = {};
 
   mapid = importData.map_data1.id;
   drawMap(importData);
-
-
-
 }
+
+$(() => {
+
+  // makes a list of the current markers
+  $.ajax ({
+          url: "/api/current-map-markers",
+          method: 'POST',
+          data: JSON.parse(map_data),
+          success: function (markers) {
+            for(let map of markers) {
+            $("<li>").data({'mapid': `${map.id}`,'title':`${map.title}`, 'description':`${map.description}`, 'picture':`${map.picture}`}).html(`${map.title} <span class="edit-remove-marker">edit</span>`).appendTo($(".map-markers-list"));
+            }
+        }
+  });
+
+  // opens the edit marker window and populates it will the markers current information
+  $(document).on('click', '.edit-remove-marker', (function() {
+    const $id = $(this).parent().data();
+    $(document).find('.update-marker').css('visibility', 'visible');
+    $(document).find('input[name="marker-name"]').val(`${$id.title}`).data({'id': `${$id.mapid}`});
+    $(document).find('textarea').text(`${$id.description}`);
+    $(document).find('input[name="marker-url"]').val(`${$id.picture}`);
+  }));
+
+  // activates the update button so that it writes new information to the database
+  $(document).on('click', '.update-button', (function(event) {
+    const $id = $(document).find('input[name="marker-name"]').data();
+      event.preventDefault();
+      var title = $('.update-marker-title').val();
+      title = title.replace("'", "");
+      var description = $('.update-marker-description').val();
+      description = description.replace("'", "");
+      let updateMarkerData = {
+        map_id: mapid,
+        id: $id.id,
+        title: $('.update-marker-title').val(),
+        description: $('.update-marker-description').val(),
+        picture: $('.update-picture-url').val()
+      };
+
+      $.ajax ({
+              url: '/update-marker',
+              method: 'POST',
+              data: updateMarkerData,
+              success: function (result) {
+              document.location.reload();
+            }
+      });
+
+  }));
+
+  // activates the delete button so that when clicked it deletes the current marker from the database
+  $(document).on('click', '.delete-button', (function(event) {
+  event.preventDefault();
+    const $id = $(document).find('input[name="marker-name"]').data();
+    let id = {id: $id.id};
+
+    $.ajax ({
+              url: '/delete-marker',
+              method: 'POST',
+              data: id,
+              success: function() {
+                document.location.reload();
+              }
+    });
+  }));
+
+  // activates the cancel button in the updata window so that when clicked it closes the update window
+  $(document).on('click', '.cancel-update-button', (function(event) {
+    event.preventDefault();
+    $(document).find('.update-marker').css('visibility', '');
+  }));
+
+});
