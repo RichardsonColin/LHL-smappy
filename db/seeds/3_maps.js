0a52b62dfc2ad10c4d5fe54068d58d157289db07
MarkZsombor <markszsombor@gmail.com>
2017-11-27 01:00:10 +0000

    updated the seed data

--- db/seeds/3_maps.js
+++ db/seeds/3_maps.js
@@ -1,8 +1,9 @@
 
 exports.seed = function(knex, Promise) {
   return knex('maps').insert([
-    {id: 1, title: 'map dummy title', user_id: 1},
-    {id: 2, title: 'Antartica pengus', user_id: 1},
+    {id: 1, title: 'First, perhaps greatest map', user_id: 1, lat: 48.425, long: -123.363, zoom: 3},
+    {id: 2, title: 'Antartica pengus', user_id: 1, lat: 82.8628, long: 135, zoom: 3},
     {id: 3, title: 'Spots I saw animals', user_id: 1, lat: 48.425, long: -123.3630, zoom: 14}
   ]);
 };
+
