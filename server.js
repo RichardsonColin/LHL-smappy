7fdda3aaac5764f06c19cb442c1b1fc897ece297
Kelsey Cooper <kcoop@live.ca>
2017-11-27 17:57:43 +0000

    cleaned up server.js

--- server.js
+++ server.js
@@ -29,13 +29,9 @@ const currentMapMarkers = require("./routes/current-map-markers");
 
 
 app.set("view engine", "ejs");
-
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
-// Log knex SQL queries to STDOUT as well
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(knexLogger(knex));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/styles", sass({
@@ -44,7 +40,8 @@ app.use("/styles", sass({
   debug: true,
   outputStyle: 'compressed'
 }));
-app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Sets the secure cookie session
 app.use(cookieSession({
   name: 'session',
   keys: ['Cleo'],
@@ -52,6 +49,7 @@ app.use(cookieSession({
   // Cookie Options
   maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }));
+
 app.use(flash());
 
 app.use(express.static("public"));
@@ -64,69 +62,8 @@ app.use("/api/contributions", contributionsRoutes(knex));
 app.use("/api/new-favourite", newFavoriteRoutes(knex));
 app.use("/api/current-map-markers", currentMapMarkers(knex));
 
-// Home page
-app.get("/", (req, res) => {
-  let loggedIn = false;
 
-  if (req.session.user_id) {
-    loggedIn = true;
-  }
-   let templateVars = {
-                       loggedIn: loggedIn,
-                       errors: req.flash('error')
-                      };
-  res.render("index", templateVars
-  );
-});
-
-
-app.get("/profile", (req, res) => {
-  let loggedIn = false;
-  if (req.session.user_id) {
-    loggedIn = true;
-    let templateVars = {
-                       loggedIn: loggedIn,
-                       userid: req.session.user_id,
-                       errors: req.flash('error')
-                      };
-  res.render("profile", templateVars
-  );
-
-
-  } else {
-    res.redirect("/");
-  }
-});
-
-app.get("/new-map", (req, res) => {
-  let loggedIn = false;
-  if (req.session.user_id) {
-    loggedIn = true;
-  }
-  let templateVars = {
-                       loggedIn: loggedIn,
-                       userid: req.session.user_id,
-                       errors: req.flash('error')
-
-                       };
-
-  res.render("new-map", templateVars);
-});
-
-function grabUserName(email){
-  knex.select('name').from('users')
-  .where('email' , email)
-  .asCallback(function(err, rows) {
-
-    if (err) return console.error(err);
-    console.log(rows);
-   return rows;
-     });
-
-}
-
-
-
+// Inserts into map contributions
 function mapContributions(contributeMapData) {
   return knex('contributions')
     .insert({
@@ -134,62 +71,35 @@ function mapContributions(contributeMapData) {
       user_id: contributeMapData.user_id
     })
     .then(() => {
-      return;
-    });
- }
+    return;
+  });
+}
 
+// Creates new map
 function createNewMap(data) {
   return knex('maps')
-    .insert(data)
-    .returning('*')
-    .then((mapData) => {
-      let mapContribute = mapData[0];
-      let mapId = mapData[0].id;
-      mapContributions(mapContribute);
-      return mapId;
+  .insert(data)
+  .returning('*')
+  .then((mapData) => {
+    let mapContribute = mapData[0];
+    let mapId = mapData[0].id;
+    mapContributions(mapContribute);
+    return mapId;
   });
 }
 
-
-app.post("/new-map", (req, res) => {
-  let newMapData = req.body;
-  newMapData.user_id = req.session.user_id;
-
-  createNewMap(newMapData).then(result => {
-    res.send(String(result));
-  });
-});
-
-
-
-
-// TO DO MOVE THIS TO HELP FUNCTIONS
-
-function registerUser(email, password) {
-  let user_id = 0;
-  return knex('users')
-    .insert({
-      email: email,
-      password: password
-    })
-    .returning('*')
-    .then((users) => {
-      user_id = users[0].id;
-      return user_id;
-    });
-}
-
-
+// Check if email is already in db before login/register
 function checkEmailInDB(email) {
   return knex
-    .select('email')
-    .from('users')
-    .where({'email': email})
-    .then((users) => {
-      return users.length !== 0;
-    });
+  .select('email')
+  .from('users')
+  .where({'email': email})
+  .then((users) => {
+    return users.length !== 0;
+  });
 }
 
+// Verifies email and password match
 function checkLogin(emailreq, password) {
   return knex('users')
   .where({ email: emailreq })
@@ -201,15 +111,143 @@ function checkLogin(emailreq, password) {
     else {
       return 0;
     }
+  });
+}
 
+// Register new urser
+function registerUser(email, password) {
+  let user_id = 0;
+  return knex('users')
+  .insert({
+    email: email,
+    password: password
+  })
+  .returning('*')
+  .then((users) => {
+    user_id = users[0].id;
+    return user_id;
+  });
+}
+
+// Create new map marker
+function createNewMarker(data) {
+  return knex('markers')
+  .insert(data)
+  .returning('*')
+  .then((markerData) => {
+    let mapId = markerData[0].map_id;
+    let userId = markerData[0].user_id;
+    let contributionsData = {
+      'id': mapId,
+      'user_id': userId
+    };
+    mapContributions(contributionsData);
+    return mapId;
+  });
+}
+
+// Update marker information
+function updateMarker(data) {
+  return knex('markers')
+  .where('id', data.id)
+  .update({
+    map_id: data.map_id,
+    title: data.title,
+    description: data.description,
+    picture: data.picture
+  })
+  .returning('*')
+  .then((markerData) => {
+    let mapId = markerData[0].map_id;
+    return mapId;
+  });
+}
+
+// Delete marker from db
+function deleteMarker(id) {
+  return knex('markers')
+  .where('id', id)
+  .del()
+  .then(() => {
+    return;
+  });
+}
+
+// Remove favourite from favourite list
+function deleteFavourite(mapId, UserId) {
+  return knex('favourite_maps')
+  .where({'map_id': mapId, 'user_id': UserId })
+  .del()
+  .then(() => {
+    return;
   });
 }
 
 
-app.get("/profile", (req, res) => {
-  res.render("profile");
+// Home page
+app.get("/", (req, res) => {
+  let loggedIn = false;
+  if (req.session.user_id) {
+    loggedIn = true;
+  }
+  let templateVars = {
+                      loggedIn: loggedIn,
+                      errors: req.flash('error')
+                      };
+  res.render("index", templateVars
+  );
 });
 
+
+// Profile page
+app.get("/profile", (req, res) => {
+  let loggedIn = false;
+  if (req.session.user_id) {
+    loggedIn = true;
+    let templateVars = {
+                       loggedIn: loggedIn,
+                       userid: req.session.user_id,
+                       errors: req.flash('error')
+                       };
+  res.render("profile", templateVars
+  );
+  } else {
+    res.redirect("/");
+  }
+});
+
+// Create map page
+app.get("/new-map", (req, res) => {
+  let loggedIn = false;
+  if (req.session.user_id) {
+    loggedIn = true;
+  }
+  let templateVars = {
+                       loggedIn: loggedIn,
+                       userid: req.session.user_id,
+                       errors: req.flash('error')
+                       };
+  res.render("new-map", templateVars);
+});
+
+
+
+///////////////HTTP REQUESTS
+
+
+
+// Post new map
+app.post("/new-map", (req, res) => {
+  let newMapData = req.body;
+  newMapData.user_id = req.session.user_id;
+
+  createNewMap(newMapData).then(result => {
+    res.send(String(result));
+  });
+});
+
+
+// Get map by map id
 app.get("/maps/:id", (req, res) => {
   let loggedIn = false;
   if (req.session.user_id) {
@@ -218,24 +256,26 @@ app.get("/maps/:id", (req, res) => {
   let mapData = {};
   let markersData = {};
 
+  // Pull map id
   function getMapData(id) {
     return knex
-      .select()
-      .from('maps')
-      .where('id', id)
-      .then((maps) => {
-        mapData = maps[0];
-        return id;
-      });
+    .select()
+    .from('maps')
+    .where('id', id)
+    .then((maps) => {
+      mapData = maps[0];
+      return id;
+    });
   }
 
+  // Pull Markers
   function getMarkers(id) {
     return knex
-      .select()
-      .from('markers')
-      .where('map_id', id)
-      .then((markers) => {
-        markersData = markers;
+    .select()
+    .from('markers')
+    .where('map_id', id)
+    .then((markers) => {
+      markersData = markers;
     });
   }
 
@@ -250,21 +290,21 @@ app.get("/maps/:id", (req, res) => {
         dataTemplate = JSON.stringify(dataTemplate);
         res.render('map_page', {data: dataTemplate, errors: req.flash('error'), loggedIn: loggedIn});
       });
-    } else {
-      res.status(404).send("Map doesn't exist");
+      } else {
+        res.status(404).send("Map doesn't exist");
     }
   });
 });
 
 
 
-
+// Log user out
 app.post("/logout", (req, res) => {
   req.session.user_id = null;
   res.redirect(req.get('referer'));
 });
 
-
+// Log user in
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
@@ -272,32 +312,31 @@ app.post('/login', (req, res) => {
     req.flash('error', 'Both email and password are required');
     res.redirect(req.get('referer'));
     return;
+  }
+  checkEmailInDB(email, password)
+  .then(exists => {
+    if (exists) {
+      checkLogin(email, password)
+      .then(exists => {
+        if (exists) {
+          req.session.user_id = exists;
+          res.redirect(req.get('referer'));
+        }
+        else {
+          req.flash('error', 'Email and password do not match');
+          res.redirect(req.get('referer'));
+          return;
+        }
+      });
+    } else {
+      req.flash('error', 'Email is not registered');
+      res.redirect(req.get('referer'));
+      return;
     }
-    checkEmailInDB(email, password)
-    .then(exists => {
-      if (exists) {
-        checkLogin(email, password)
-        .then(exists => {
-          if (exists) {
-            req.session.user_id = exists;
-            res.redirect(req.get('referer'));
-          }
-          else {
-            req.flash('error', 'Email and password do not match');
-            res.redirect(req.get('referer'));
-            return;
-          }
-        });
-      }
-      else {
-        req.flash('error', 'Email is not registered');
-        res.redirect(req.get('referer'));
-        return;
-      }
   });
 });
 
-
+// Register user
 app.post('/register', (req, res) => {
 
   const email = req.body.email;
@@ -322,25 +361,8 @@ app.post('/register', (req, res) => {
   });
 });
 
-function createNewMarker(data) {
-  return knex('markers')
-    .insert(data)
-    .returning('*')
-    .then((markerData) => {
-      let mapId = markerData[0].map_id;
-      let userId = markerData[0].user_id;
-      let contributionsData = {
-        'id': mapId,
-        'user_id': userId
-      };
-
-      mapContributions(contributionsData);
-      return mapId;
-    });
-}
-
+// Create new marker
 app.post("/new-marker", (req, res) => {
-
   let newMapData = req.body;
   newMapData.user_id = req.session.user_id;
   createNewMarker(newMapData).then(result => {
@@ -348,24 +370,9 @@ app.post("/new-marker", (req, res) => {
   });
 });
 
-function updateMarker(data) {
-  return knex('markers')
-    .where('id', data.id)
-    .update({
-      map_id: data.map_id,
-      title: data.title,
-      description: data.description,
-      picture: data.picture
-    })
-    .returning('*')
-    .then((markerData) => {
-      let mapId = markerData[0].map_id;
-      return mapId;
-    });
-}
 
+// Update marker
 app.post("/update-marker", (req, res) => {
-
   let updateMarkerData = req.body;
   updateMarkerData.user_id = req.session.user_id;
   updateMarker(updateMarkerData).then(result => {
@@ -373,44 +380,15 @@ app.post("/update-marker", (req, res) => {
   });
 });
 
-function deleteMarker(markerId, userId) {
-  return knex('markers')
-    .where('id', markerId)
-    .del()
-    .then(() => {
-      deleteContribution(markerId, userId);
-      return;
-    });
-}
-
-function deleteContribution(id) {
-  return knex('contributions')
-    .where('id', id)
-    .del()
-    .then(() => {
-
-      return;
-    });
-}
-
+// Remove marker
 app.post("/delete-marker", (req, res) => {
   let markerId = req.body.id;
-  let userId = req.session.user_id;
-  deleteMarker(markerId, userId).then(result => {
+  deleteMarker(markerId).then(result => {
     res.send('success');
   });
 });
 
-function deleteFavourite(mapId, UserId) {
-
-  return knex('favourite_maps')
-    .where({'map_id': mapId, 'user_id': UserId })
-    .del()
-    .then(() => {
-      return;
-  });
-}
-
+// Remove Favourite
 app.post("/remove-favourite", (req, res) => {
   console.log('I AM THE REMOVE FAVOURITE ID',req.body);
   let mapId = req.body.id;
@@ -421,7 +399,7 @@ app.post("/remove-favourite", (req, res) => {
   });
 });
 
-
+//Update profile info
 app.post('/profile-update', (req, res) => {
    return knex('users')
    .where({id: req.session.user_id})
@@ -433,6 +411,9 @@ app.post('/profile-update', (req, res) => {
   res.send(String(mapData));
   });
 });
+
+
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
