4f5b3d0ed8e739824c15b0e785805a3540074ee7
MarkZsombor <markszsombor@gmail.com>
2018-02-22 23:50:28 +0000

    removed dead code and console logs

--- routes/current-map-markers.js
+++ routes/current-map-markers.js
@@ -5,7 +5,6 @@ module.exports = (knex) => {
 
   // Displays markers if any are created.
   router.post("/", (req, res) => {
-    console.log("data fed into the current map markesrs route", req.body)
     knex
     .select('*')
     .from('markers')
--- routes/getMap.js
+++ routes/getMap.js
@@ -5,15 +5,12 @@ module.exports = (knex) => {
 
   // Gets the map data.
   router.post("/", (req, res) => {
-    console.log("data fed into the get map route", req.body)
     knex
     .select('*')
     .from('maps')
-    // .where('id', req.params.id)
     .where('id', req.body.id)
     .then((results) => {
       if(results) {
-        console.log("Map data in api route", results);
         res.json(results[0]);
       } else {
         res.status(404).send("Map doesn't exist");
