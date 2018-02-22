896f9c4c0585cadda22e04ec750adacc8288ebed
MarkZsombor <markszsombor@gmail.com>
2018-02-22 02:28:51 +0000

    fixed seeding issue

--- db/seeds/2_users.js
+++ db/seeds/2_users.js
@@ -3,7 +3,7 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice Cooper', email: 'alice@coop.ca', password: 'password', your_location: 'Victoria', your_description: 'Cool dude'})
+        knex('users').insert({name: 'Alice Cooper', email: 'alice@coop.ca', password: 'password', your_location: 'Victoria', your_description: 'Cool dude'})
       ]);
     });
 };
